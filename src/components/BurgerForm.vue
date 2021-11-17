665f985cb5a77340f09e668e825291792d76868f
JNR23 <junior_bhzte@hotmail.com>
2021-11-17 10:17:38 -0300

    Creating system messages component

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <p>Compomente de Mensagem</p>
+        <Message :msg="msg" v-show="msg" />
         <div>
             <form id="burger-form" @submit="createBurger">
                 <div class="input-container">
@@ -51,8 +51,13 @@
 </template>
 
 <script>
+import Message from './Message.vue';
+
 export default {
     name: "BurgerForm",
+    components: {
+        Message
+    },
     data() {
         return {
             paes: null,
@@ -94,6 +99,10 @@ export default {
             });
 
             const res = await req.json();
+
+            this.msg = `Pedido Nº ${res.id} realizado com sucesso`;
+
+            setTimeout(() => this.msg = "", 3000);
 
             this.nome = "";
             this.carne = "";
